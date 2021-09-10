95e54b27513ef86b7a7a0defe4977f7d7c692d6c
Aline Silva <line_adri@hotmail.com>
2021-09-10 18:40:16 -0300

    excluindo index.js e editando routs

--- src/routes/idoso.routes.js
+++ src/routes/idoso.routes.js
@@ -5,6 +5,14 @@ const router = express.Router()
 const controllers = require('../controllers/idosoController')
 
 
+//msg de apresentação
+router.get('/', (req, res)=>{
+    res.status(200).json({
+        "tittle": "Vem aqui",
+        "version": "1.0.0",
+        "mensagem": "Um app funcional para mapear idosos que precisam de ajuda no dia a dia. "
+    })
+})
 
 // READ - listar todos os idosos cadastrados  -> get -> find
 router.get('/', controllers.getAll)
