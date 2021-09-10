22be4ff548624e5bc16abf34cc8a96336ce844c7
Aline Silva <line_adri@hotmail.com>
2021-09-10 18:21:10 -0300

    criando index.js

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router()
+
+router.get('/', (req, res)=>{
+    res.status(200).json({
+        "tittle": "Vem aqui",
+        "version": "1.0.0",
+        "mensagem": "Um app funcional para mapear idosos que precisam de ajuda no dia a dia. "
+    })
+})
+
+module.exports = router
